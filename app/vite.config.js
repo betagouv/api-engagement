6df809d6d17ea6c9627fbbc30a7f7a25ed472e2c
theolemague <theo.lm@hotmail.fr>
2024-11-14 16:41:11 +0100

    try source maps sentry

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,17 +1,45 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig((env) => {
+  const plugins = [react(), svgr()];
+
+  if (env.mode === "production") {
+    plugins.push(
+      sentryVitePlugin({
+        org: "sentry",
+        project: "api-engagement-production",
+        url: "https://sentry.selego.co/",
+        environment: "app",
+      }),
+    );
+  } else if (env.mode === "staging") {
+    plugins.push(
+      sentryVitePlugin({
+        org: "sentry",
+        project: "api-engagement-staging",
+        url: "https://sentry.selego.co/",
+        environment: "app",
+      }),
+    );
+  }
+
+  return {
+    server: {
+      port: 3000,
     },
-  },
-  plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins,
+    build: {
+      sourcemap: true,
+    },
+  };
 });
