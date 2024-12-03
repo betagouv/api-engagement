577ee11e5951941124018d14b940928ca5e78a11
theolemague <theo.lm@hotmail.fr>
2024-12-03 16:06:01 +0100

    fix:cron report

--- process/src/index.ts
+++ process/src/index.ts
@@ -279,7 +279,7 @@ const leboncoinJob = new CronJob(
 
 // Every first Tuesday of the month at 10:00 AM
 const reportJob = new CronJob(
-  "1 16 * * 2",
+  "10 16 * * 2",
   async () => {
     // if not the first Tuesday of the month, return
     const date = new Date();
