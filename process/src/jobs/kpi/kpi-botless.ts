691f53626c059bde6f82d65a1822dbb9dbe8b273
theolemague <theo.lm@hotmail.fr>
2025-01-24 11:50:21 +0100

    create branch

---
+++ process/src/jobs/kpi/kpi-botless.ts
@@ -0,0 +1,198 @@
+import { STATS_INDEX } from "../../config";
+import esClient from "../../db/elastic";
+import KpiBotlessModel from "../../models/kpi-botless";
+import MissionModel from "../../models/mission";
+import StatsBotModel from "../../models/stats-bot";
+import { Kpi } from "../../types";
+
+// Cron that create a kpi doc every with the data available
+export const buildKpiBotless = async (start: Date) => {
+  const exists = await KpiBotlessModel.findOne({ date: start });
+  if (exists) return console.log(`[KPI Botless] KPI already exists for ${start.toISOString()}`);
+
+  console.log(`[KPI Botless] Starting at ${start.toISOString()}`);
+
+  // Get the previous day
+  const fromDate = new Date(start.getFullYear(), start.getMonth(), start.getDate() - 1);
+  const endDate = new Date(start.getFullYear(), start.getMonth(), start.getDate());
+
+  const kpi = { date: start } as Kpi;
+
+  const whereMissionAvailable = {
+    $or: [{ deletedAt: { $gte: fromDate } }, { deleted: false }],
+    createdAt: { $lt: endDate },
+  };
+
+  const whereBenevolatAvailable = { ...whereMissionAvailable, publisherName: { $ne: "Service Civique" } };
+  const whereVolontariatAvailable = { ...whereMissionAvailable, publisherName: "Service Civique" };
+
+  const availableBenevolatMissionCount = await MissionModel.countDocuments(whereBenevolatAvailable);
+  const availableVolontariatMissionCount = await MissionModel.countDocuments(whereVolontariatAvailable);
+
+  const aggs = await MissionModel.aggregate([
+    {
+      $facet: {
+        benevolat_given: [{ $match: { ...whereBenevolatAvailable, placesStatus: "GIVEN_BY_PARTNER" } }, { $group: { _id: null, count: { $sum: 1 }, total: { $sum: "$places" } } }],
+        volontariat_given: [
+          { $match: { ...whereVolontariatAvailable, placesStatus: "GIVEN_BY_PARTNER" } },
+          { $group: { _id: null, count: { $sum: 1 }, total: { $sum: "$places" } } },
+        ],
+        benevolat_attributed: [
+          { $match: { ...whereBenevolatAvailable, placesStatus: "ATTRIBUTED_BY_API" } },
+          { $group: { _id: null, count: { $sum: 1 }, total: { $sum: "$places" } } },
+        ],
+        volontariat_attributed: [
+          { $match: { ...whereVolontariatAvailable, placesStatus: "ATTRIBUTED_BY_API" } },
+          { $group: { _id: null, count: { $sum: 1 }, total: { $sum: "$places" } } },
+        ],
+      },
+    },
+  ]);
+
+  const availableBenevolatGivenPlaceCount = aggs.length ? aggs[0].benevolat_given[0]?.total || 0 : 0;
+  const availableVolontariatGivenPlaceCount = aggs.length ? aggs[0].volontariat_given[0]?.total || 0 : 0;
+  const availableBenevolatAttributedPlaceCount = aggs.length ? aggs[0].benevolat_attributed[0]?.total || 0 : 0;
+  const availableVolontariatAttributedPlaceCount = aggs.length ? aggs[0].volontariat_attributed[0]?.total || 0 : 0;
+
+  const availableBenevolatGivenMissionCount = aggs.length ? aggs[0].benevolat_given[0]?.count || 0 : 0;
+  const availableVolontariatGivenMissionCount = aggs.length ? aggs[0].volontariat_given[0]?.count || 0 : 0;
+  const availableBenevolatAttributedMissionCount = aggs.length ? aggs[0].benevolat_attributed[0]?.count || 0 : 0;
+  const availableVolontariatAttributedMissionCount = aggs.length ? aggs[0].volontariat_attributed[0]?.count || 0 : 0;
+
+  const percentageBenevolatGivenPlaces = availableBenevolatGivenMissionCount / availableBenevolatMissionCount;
+  const percentageVolontariatGivenPlaces = availableVolontariatGivenMissionCount / availableVolontariatMissionCount;
+  const percentageBenevolatAttributedPlaces = availableBenevolatAttributedMissionCount / availableBenevolatMissionCount;
+  const percentageVolontariatAttributedPlaces = availableVolontariatAttributedMissionCount / availableVolontariatMissionCount;
+
+  const statsBots = await StatsBotModel.find({}).lean();
+
+  const statsBenevolatAggs = await esClient.search({
+    index: STATS_INDEX,
+    body: {
+      query: {
+        bool: {
+          must_not: [
+            {
+              term: { "toPublisherName.keyword": "Service Civique" },
+            },
+            {
+              terms: { "user.keyword": statsBots.map((e) => e.user) },
+            },
+          ],
+          filter: [{ range: { createdAt: { gte: fromDate, lt: endDate } } }],
+        },
+      },
+      aggs: {
+        print: {
+          filter: { term: { "type.keyword": "print" } },
+          aggs: {
+            data: {
+              cardinality: { field: "missionId.keyword" },
+            },
+          },
+        },
+        click: {
+          filter: { term: { "type.keyword": "click" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+        apply: {
+          filter: { term: { "type.keyword": "apply" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+        account: {
+          filter: { term: { "type.keyword": "account" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+      },
+    },
+  });
+
+  const statsVolontariatAggs = await esClient.search({
+    index: STATS_INDEX,
+    body: {
+      query: {
+        bool: {
+          must_not: [
+            {
+              terms: { "user.keyword": statsBots.map((e) => e.user) },
+            },
+          ],
+          filter: [{ term: { "toPublisherName.keyword": "Service Civique" } }, { range: { createdAt: { gte: fromDate, lt: endDate } } }],
+        },
+      },
+      aggs: {
+        print: {
+          filter: { term: { "type.keyword": "print" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+        click: {
+          filter: { term: { "type.keyword": "click" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+        apply: {
+          filter: { term: { "type.keyword": "apply" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+        account: {
+          filter: { term: { "type.keyword": "account" } },
+          aggs: {
+            data: { cardinality: { field: "missionId.keyword" } },
+          },
+        },
+      },
+    },
+  });
+
+  kpi.availableBenevolatMissionCount = availableBenevolatMissionCount;
+  kpi.availableVolontariatMissionCount = availableVolontariatMissionCount;
+
+  kpi.availableBenevolatGivenPlaceCount = availableBenevolatGivenPlaceCount;
+  kpi.availableVolontariatGivenPlaceCount = availableVolontariatGivenPlaceCount;
+  kpi.availableBenevolatAttributedPlaceCount = availableBenevolatAttributedPlaceCount;
+  kpi.availableVolontariatAttributedPlaceCount = availableVolontariatAttributedPlaceCount;
+
+  kpi.percentageBenevolatGivenPlaces = percentageBenevolatGivenPlaces;
+  kpi.percentageVolontariatGivenPlaces = percentageVolontariatGivenPlaces;
+  kpi.percentageBenevolatAttributedPlaces = percentageBenevolatAttributedPlaces;
+  kpi.percentageVolontariatAttributedPlaces = percentageVolontariatAttributedPlaces;
+
+  kpi.benevolatPrintMissionCount = statsBenevolatAggs.body.aggregations.print.data.value || 0;
+  kpi.volontariatPrintMissionCount = statsVolontariatAggs.body.aggregations.print.data.value || 0;
+
+  kpi.benevolatClickMissionCount = statsBenevolatAggs.body.aggregations.click.data.value || 0;
+  kpi.volontariatClickMissionCount = statsVolontariatAggs.body.aggregations.click.data.value || 0;
+
+  kpi.benevolatApplyMissionCount = statsBenevolatAggs.body.aggregations.apply.data.value || 0;
+  kpi.volontariatApplyMissionCount = statsVolontariatAggs.body.aggregations.apply.data.value || 0;
+
+  kpi.benevolatAccountMissionCount = statsBenevolatAggs.body.aggregations.account.data.value || 0;
+  kpi.volontariatAccountMissionCount = statsVolontariatAggs.body.aggregations.account.data.value || 0;
+
+  kpi.benevolatPrintCount = statsBenevolatAggs.body.aggregations.print.doc_count || 0;
+  kpi.volontariatPrintCount = statsVolontariatAggs.body.aggregations.print.doc_count || 0;
+
+  kpi.benevolatClickCount = statsBenevolatAggs.body.aggregations.click.doc_count || 0;
+  kpi.volontariatClickCount = statsVolontariatAggs.body.aggregations.click.doc_count || 0;
+
+  kpi.benevolatApplyCount = statsBenevolatAggs.body.aggregations.apply.doc_count || 0;
+  kpi.volontariatApplyCount = statsVolontariatAggs.body.aggregations.apply.doc_count || 0;
+
+  kpi.benevolatAccountCount = statsBenevolatAggs.body.aggregations.account.doc_count || 0;
+  kpi.volontariatAccountCount = statsVolontariatAggs.body.aggregations.account.doc_count || 0;
+
+  await KpiBotlessModel.create(kpi);
+
+  console.log(`[KPI Botless] Created kpi for ${start.toISOString()}`);
+};
---
+++ process/src/jobs/metabase/kpi-botless.ts
@@ -0,0 +1,88 @@
+import { Kpi as PgKpi } from "@prisma/client";
+import prisma from "../../db/postgres";
+
+import { captureException } from "../../error";
+import { Kpi } from "../../types";
+import KpiModel from "../../models/kpi";
+
+const buildData = (doc: Kpi) => {
+  const obj = {
+    old_id: doc._id.toString(),
+    date: doc.date,
+    available_benevolat_mission_count: doc.availableBenevolatMissionCount,
+    available_volontariat_mission_count: doc.availableVolontariatMissionCount,
+
+    available_benevolat_given_by_partner_place_count: doc.availableBenevolatGivenPlaceCount,
+    available_volontariat_given_by_partner_place_count: doc.availableVolontariatGivenPlaceCount,
+
+    available_benevolat_attributed_by_api_place_count: doc.availableBenevolatAttributedPlaceCount,
+    available_volontariat_attributed_by_api_place_count: doc.availableVolontariatAttributedPlaceCount,
+
+    percentage_benevolat_given_by_partner_places: doc.percentageBenevolatGivenPlaces,
+    percentage_volontariat_given_by_partner_places: doc.percentageVolontariatGivenPlaces,
+
+    percentage_benevolat_attributed_by_api_places: doc.percentageBenevolatAttributedPlaces,
+    percentage_volontariat_attributed_by_api_places: doc.percentageVolontariatAttributedPlaces,
+
+    benevolat_print_mission_count: doc.benevolatPrintMissionCount,
+    volontariat_print_mission_count: doc.volontariatPrintMissionCount,
+
+    benevolat_click_mission_count: doc.benevolatClickMissionCount,
+    volontariat_click_mission_count: doc.volontariatClickMissionCount,
+
+    benevolat_apply_mission_count: doc.benevolatApplyMissionCount,
+    volontariat_apply_mission_count: doc.volontariatApplyMissionCount,
+
+    benevolat_account_mission_count: doc.benevolatAccountMissionCount,
+    volontariat_account_mission_count: doc.volontariatAccountMissionCount,
+
+    benevolat_print_count: doc.benevolatPrintCount,
+    volontariat_print_count: doc.volontariatPrintCount,
+
+    benevolat_click_count: doc.benevolatClickCount,
+    volontariat_click_count: doc.volontariatClickCount,
+
+    benevolat_apply_count: doc.benevolatApplyCount,
+    volontariat_apply_count: doc.volontariatApplyCount,
+
+    benevolat_account_count: doc.benevolatAccountCount,
+    volontariat_account_count: doc.volontariatAccountCount,
+  } as PgKpi;
+  return obj;
+};
+
+const handler = async () => {
+  try {
+    const start = new Date();
+    console.log(`[KPI] Starting at ${start.toISOString()}`);
+
+    const data = await KpiModel.find().lean();
+    console.log(`[KPI] Found ${data.length} docs to sync.`);
+
+    const stored = [] as string[];
+    await prisma.kpiBotLess.findMany({ select: { old_id: true } }).then((data) => data.forEach((d) => stored.push(d.old_id)));
+    console.log(`[KPI] Found ${stored.length} docs in database.`);
+
+    const dataToCreate = [];
+    for (const doc of data) {
+      const exists = stored.includes(doc._id.toString());
+      if (exists) continue;
+      const obj = buildData(doc);
+      dataToCreate.push(obj);
+    }
+
+    // Create data
+    if (dataToCreate.length) {
+      console.log(`[KPI] Creating ${dataToCreate.length} docs...`);
+      const res = await prisma.kpiBotLess.createMany({ data: dataToCreate, skipDuplicates: true });
+      console.log(`[KPI] Created ${res.count} docs.`);
+    }
+
+    console.log(`[KPI] Ended at ${new Date().toISOString()} in ${(Date.now() - start.getTime()) / 1000}s.`);
+    return { created: dataToCreate.length };
+  } catch (error) {
+    captureException(error, "[KPI] Error while syncing docs.");
+  }
+};
+
+export default handler;
