d0380f9bc27ae164f275e520f45466e57b21e770
theolemague <theo.lm@hotmail.fr>
2025-03-11 16:02:19 +0100

    clean code

--- api/src/models/moderation-event.ts
+++ api/src/models/moderation-event.ts
@@ -5,73 +5,22 @@ import { ModerationEvent } from "../types";
 const MODELNAME = "moderation-event";
 const schema = new Schema<ModerationEvent>(
   {
-    missionId: {
-      type: String,
-      required: true,
-    },
-
-    moderatorId: {
-      type: String,
-      required: true,
-    },
-
-    userId: {
-      type: String,
-    },
-
-    userName: {
-      type: String,
-    },
-
-    initialStatus: {
-      type: String,
-      enum: ["ACCEPTED", "REFUSED", "PENDING", "ONGOING"],
-    },
-
-    newStatus: {
-      type: String,
-      enum: ["ACCEPTED", "REFUSED", "PENDING", "ONGOING", ""],
-    },
-
-    initialTitle: {
-      type: String,
-    },
-
-    newTitle: {
-      type: String,
-    },
-
-    initialComment: {
-      type: String,
-    },
-
-    newComment: {
-      type: String,
-    },
-
-    initialNote: {
-      type: String,
-    },
-
-    newNote: {
-      type: String,
-    },
-
-    initialSiren: {
-      type: String,
-    },
-
-    newSiren: {
-      type: String,
-    },
-
-    initialRNA: {
-      type: String,
-    },
-
-    newRNA: {
-      type: String,
-    },
+    missionId: { type: String, required: true },
+    moderatorId: { type: String, required: true },
+    userId: { type: String },
+    userName: { type: String },
+    initialStatus: { type: String, enum: ["ACCEPTED", "REFUSED", "PENDING", "ONGOING"] },
+    newStatus: { type: String, enum: ["ACCEPTED", "REFUSED", "PENDING", "ONGOING", ""] },
+    initialTitle: { type: String },
+    newTitle: { type: String },
+    initialComment: { type: String },
+    newComment: { type: String },
+    initialNote: { type: String },
+    newNote: { type: String },
+    initialSiren: { type: String },
+    newSiren: { type: String },
+    initialRNA: { type: String },
+    newRNA: { type: String },
   },
   {
     timestamps: true,
