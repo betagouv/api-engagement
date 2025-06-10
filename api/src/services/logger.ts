2d0d3c92707f3de7683158630fb2ad36e8d119c8
theolemague <theo.lm@hotmail.fr>
2025-06-10 14:59:13 +0200

    Update logger.ts

--- api/src/services/logger.ts
+++ api/src/services/logger.ts
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 import morgan from "morgan";
-import { User } from "../types";
+import { Publisher, User } from "../types";
 
 const METHOD_COLORS = {
   GET: "green",
@@ -15,17 +15,19 @@ const logger = () => {
     const methodColor = METHOD_COLORS[method] || "white";
     const statusColor = res.statusCode >= 500 ? "red" : res.statusCode >= 400 ? "yellow" : "green";
 
-    const user = (req as any).user as User;
+    const user = (req as any).user as User | Publisher;
     // hide GET / requests
     if (tokens.method(req, res) === "GET" && tokens.url(req, res) === "/") {
       return null;
     }
 
+    const isUser = "firstname" in user;
+
     return [
       chalk[methodColor].bold(method),
       chalk.white.bold(tokens.url(req, res)),
       chalk[statusColor].bold(tokens.status(req, res)),
-      `- ${tokens["response-time"](req, res)}ms ${user && user._id && `user-id:${user._id}`}`,
+      `- ${tokens["response-time"](req, res)}ms ${user ? (isUser ? `user-id:${user._id}` : `publisher-id:${user._id}`) : ""}`,
     ].join(" ");
   });
 };
