5b99640aacd91d953a9de0b61e57c3f2ace41c01
theolemague <theo.lm@hotmail.fr>
2025-06-19 11:25:17 +0200

    clean logs

--- api/src/middlewares/rate-limite.ts
+++ api/src/middlewares/rate-limite.ts
@@ -7,20 +7,13 @@ const limiter = rateLimit({
   windowMs: 5000, // 5 second
   limit: 15, // Limit each IP to 15 requests per `window` (5 r/s).
   skip: (req) => {
-    // if request is from BENEVOLAT_URL or VOLONTARIAT_URL, skip rate limiting
-
-    // Letting the console.log here to test the ip from the widget
-    console.log("req.headers.referer", req.headers.origin, req.ip);
+    // Skip back office and iframe requests
     if ([APP_URL, BENEVOLAT_URL, VOLONTARIAT_URL].includes(req.headers.origin || "")) {
-      console.log("skipping rate limiting");
       return true;
     }
-    console.log("req.url", req.url);
     if (req.url.includes("/iframe/")) {
-      console.log("skipping rate limiting iframe");
       return true;
     }
-    console.log("not skipping rate limiting");
     return false;
   },
   handler: (req, res) => {
