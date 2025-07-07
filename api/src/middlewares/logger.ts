14bdf03c0e608b167e47587fe584710b72c3cf4c
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2025-07-07 12:03:01 +0200

    chore: Better logging data

--- api/src/middlewares/logger.ts
+++ api/src/middlewares/logger.ts
@@ -3,40 +3,46 @@ import morgan from "morgan";
 
 const SENSITIVE_FIELDS = ["password", "apikey"];
 
-const logger = morgan<Request, Response>((tokens, req, res) => {
-  const user = req.user;
-  const isUser = user ? "firstname" in user : false;
+const logger = morgan<Request, Response>(
+  (tokens, req, res) => {
+    const user = req.user;
+    const isUser = user ? "firstname" in user : false;
 
-  const log: { [key: string]: any } = {
-    method: tokens.method(req, res),
-    path: req.path,
-    query: req.query,
-    status: tokens.status(req, res),
-    "response-time": tokens["response-time"](req, res),
-  };
+    const log: { [key: string]: any } = {
+      method: tokens.method(req, res),
+      path: req.originalUrl.split("?")[0],
+      endpoint: req.route ? req.baseUrl + req.route.path : undefined,
+      query: req.query,
+      status: tokens.status(req, res),
+      "response-time": Math.round(parseFloat(tokens["response-time"](req, res) || "0")),
+    };
 
-  if (user) {
-    if (isUser) {
-      log.user_id = user._id;
-    } else {
-      log.publisher_id = user._id;
-    }
-  }
-
-  const method = tokens.method(req, res);
-  if ((method === "POST" || method === "PUT") && req.body) {
-    const bodyToLog = { ...req.body };
-
-    SENSITIVE_FIELDS.forEach((field) => {
-      if (bodyToLog[field]) {
-        bodyToLog[field] = "****";
+    if (user) {
+      if (isUser) {
+        log.user_id = user._id;
+      } else {
+        log.publisher_id = user._id;
       }
-    });
+    }
 
-    log.body = bodyToLog;
+    const method = tokens.method(req, res);
+    if ((method === "POST" || method === "PUT") && req.body) {
+      const bodyToLog = { ...req.body };
+
+      SENSITIVE_FIELDS.forEach((field) => {
+        if (bodyToLog[field]) {
+          bodyToLog[field] = "****";
+        }
+      });
+
+      log.body = bodyToLog;
+    }
+
+    return JSON.stringify(log);
+  },
+  {
+    skip: (req) => req.method === "HEAD",
   }
-
-  return JSON.stringify(log);
-});
+);
 
 export default logger;
