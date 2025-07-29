d844ad4af3c596abf05545d570828951fe04a5b9
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2025-07-29 14:07:37 +0200

    Migrate  v2/mission to v0/mission (#353)
    * Migrate v2/mission to v0/mission
    
    * Fix test

--- api/tests/integration/api/v0/mission.test.ts
+++ api/tests/integration/api/v0/mission.test.ts
@@ -287,6 +287,13 @@ describe("Mission API Integration Tests", () => {
       expect(response.body.data[0].reducedMobilityAccessible).toBe("no");
     });
 
+    it("should filter by snu", async () => {
+      await createTestMission({ publisherId: publisher.publishers[0].publisherId, snu: true });
+      const response = await request(app).get("/v0/mission/?snu=true").set("x-api-key", apiKey);
+      expect(response.status).toBe(200);
+      expect(response.body.total).toBe(1);
+    });
+
     it("should filter by type", async () => {
       await createTestMission({ publisherId: publisher.publishers[0].publisherId, type: MissionType.VOLONTARIAT });
       const response = await request(app).get(`/v0/mission?type=${MissionType.VOLONTARIAT}`).set("x-api-key", apiKey);
