fdf86ed4c87bb66bacc6cc21e4b4b139313710aa
theolemague <theo.lm@hotmail.fr>
2025-08-19 16:51:01 +0200

    push

--- widget/components/CardBenevolat.jsx
+++ widget/components/CardBenevolat.jsx
@@ -39,57 +39,48 @@ const Card = ({ widget, mission, request, focused = false, onKeyDown = null, onR
     <div
       data-testid="mission-card"
       className={`${
-        widget.style === "carousel"
-          ? "max-h-[420px] min-h-[420px] w-full lg:max-w-[336px] xl:max-h-[460px] xl:min-h-[460px]"
-          : "max-h-[420px] min-h-[420px] w-full lg:max-h-[440px] lg:min-h-[440px]"
+        widget.style === "carousel" ? "w-full lg:max-w-[336px]" : "w-full"
       } relative group mx-auto flex h-full flex-col overflow-hidden border border-[#DDDDDD] transition-shadow duration-300 hover:shadow-lg focus-within:ring-2 focus-within:ring-[#000091] focus-within:ring-offset-0`}
     >
-      <div className="max-h-[188px] min-h-[188px] overflow-hidden xl:max-h-[200px] xl:min-h-[200px]">
+      <div className="h-[192px] sm:h-[200px] overflow-hidden">
         <Image
+          alt=""
           src={error ? "/generique.jpeg" : mission.domainLogo}
           priority={true}
           className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110 group-focus-within:scale-110"
-          width={500}
-          height={500}
+          width={200}
+          height={200}
           onError={() => setError(true)}
         />
       </div>
 
-      <div className="flex flex-1 flex-col justify-between p-6">
-        <div>
-          <div className="mb-4 flex flex-col gap-2">
-            <span className="w-fit rounded-full bg-[#EEE] px-2 py-0.5 text-xs">{domain.label || mission.domain}</span>
-            <div className="flex gap-2 text-[#666]">
-              <RiBuildingFill />
-              <span className="line-clamp-1 text-xs">{mission.organizationName}</span>
-            </div>
-          </div>
-
-          <div className="mb-4 h-[4.5rem]">
-            <a
-              ref={ref}
-              href={mission.url}
-              target="_blank"
-              className="after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 focus:outline-none"
-              tabIndex={widget.style === "carousel" ? (focused ? 0 : -1) : undefined}
-              aria-focused={widget.style === "carousel" ? focused : undefined}
-              onKeyDown={onKeyDown}
-            >
-              <h2 className="line-clamp-3 text-xl font-semibold leading-tight transition-colors duration-300 group-hover:text-[#000091] group-focus-within:text-[#000091]">
-                {mission.title}
-              </h2>
-            </a>
-          </div>
-
-          <div className="mb-4 flex flex-col">
-            <span className="mb-2 line-clamp-1 text-sm text-[#3A3A3A]">{address}</span>
-            <span className="text-xs text-[#666666]">{`${mission.places} ${mission.places > 1 ? "bénévoles recherchés" : "bénévole recherché"}`}</span>
-          </div>
-        </div>
-
-        <div className="mt-4 w-full text-center">
+      <div className="h-[276px] sm:h-[292px] flex flex-col justify-between p-6 sm:p-8 gap-4">
+        <div className="flex-1 overflow-hidden">
           <span name="tracker_counter" data-id={mission._id} data-publisher={widget.fromPublisherId.toString()} data-source={widget._id.toString()} data-request={request} />
+          <div className="w-fit flex items-center rounded-full bg-[#EEE] px-2 h-6 text-xs leading-5 mb-3">{domain.label || mission.domain}</div>
+          <div className="flex items-center gap-2 text-[#666] mb-4">
+            <RiBuildingFill />
+            <span className="line-clamp-1 text-xs leading-5">{mission.organizationName}</span>
+          </div>
+          <a
+            ref={ref}
+            href={mission.url}
+            target="_blank"
+            className="after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 focus:outline-none"
+            tabIndex={widget.style === "carousel" ? (focused ? 0 : -1) : undefined}
+            aria-focused={widget.style === "carousel" ? focused : undefined}
+            onKeyDown={onKeyDown}
+          >
+            <h2 className="line-clamp-3 text-xl text-[#161616] font-semibold leading-7 transition-colors duration-300 group-hover:text-[#000091] group-focus-within:text-[#000091]">
+              {mission.title}
+            </h2>
+          </a>
+          <p className="mt-3 line-clamp-1 group-hover:line-clamp-2 group-focus-within:line-clamp-2 text-sm text-[#3A3A3A] leading-6">{address}</p>
         </div>
+
+        <p
+          className={`text-xs ${address.length > 60 ? "group-hover:hidden group-focus-within:hidden" : ""} text-[#666666] leading-5`}
+        >{`${mission.places} ${mission.places > 1 ? "bénévoles recherchés" : "bénévole recherché"}`}</p>
       </div>
     </div>
   );
--- widget/components/CardVolontariat.jsx
+++ widget/components/CardVolontariat.jsx
@@ -40,15 +40,14 @@ const CardVolontariat = ({ widget, mission, request, focused = false, onKeyDown 
       data-testid="mission-card"
       className={`relative ${
         widget.style === "carousel" ? "w-full lg:max-w-[336px]" : "w-full"
-      } relative border min-h-[290px] max-h-[290px] md:min-h-[311px] md:max-h-[311px] flex flex-col border-[#DDDDDD] bg-white group overflow-hidden focus-within:ring-2 focus-within:ring-[#000091] focus-within:ring-offset-0`}
+      } relative border h-[290px] md:h-[310px] flex flex-col p-6 md:p-8 border-[#DDDDDD] bg-white group overflow-hidden focus-within:ring-2 focus-within:ring-[#000091] focus-within:ring-offset-0`}
     >
-      <div className="flex-1 flex flex-col p-5 gap-3">
-        <div className="flex items-start min-w-0">
-          <div className="flex-shrink-0">
-            <div className="w-10 h-10 rounded-full flex items-center justify-center text-white text-2xl" style={{ background: domain.color }} data-icon={domain.icon} />
-          </div>
+      <div className="flex-1">
+        <div className="flex items-start mb-4">
+          <div className="w-10 h-10 rounded-full flex items-center justify-center text-white text-2xl" style={{ background: domain.color }} data-icon={domain.icon} />
+
           <div className="ml-3 flex-1 min-w-0" style={{ color: domain.color }}>
-            <p className="uppercase font-bold text-sm whitespace-nowrap truncate tracking-wider block">{domain.label || mission.domain}</p>
+            <p className="uppercase font-bold text-sm leading-5 whitespace-nowrap truncate tracking-wider">{domain.label || mission.domain}</p>
             <div className="text-[#666666] flex items-center">
               <RiBuildingFill className="text-sm flex-shrink-0" />
               <p className="ml-2 text-xs line-clamp-1">{mission.organizationName}</p>
@@ -56,43 +55,37 @@ const CardVolontariat = ({ widget, mission, request, focused = false, onKeyDown 
           </div>
         </div>
 
-        <div className="flex flex-col gap-3 flex-grow">
-          <div className="w-full text-center">
-            <span name="tracker_counter" data-id={mission._id} data-publisher={widget.fromPublisherId} data-source={widget._id} data-request={request} />
-          </div>
+        <div className="w-full text-center">
+          <span name="tracker_counter" data-id={mission._id} data-publisher={widget.fromPublisherId} data-source={widget._id} data-request={request} />
+        </div>
 
-          <a
-            ref={ref}
-            href={mission.url}
-            target="_blank"
-            className="after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 focus:outline-none"
-            tabIndex={widget.style === "carousel" ? (focused ? 0 : -1) : undefined}
-            aria-focused={widget.style === "carousel" ? focused : undefined}
-            onKeyDown={onKeyDown}
+        <a
+          ref={ref}
+          href={mission.url}
+          target="_blank"
+          className="after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 focus:outline-none"
+          tabIndex={widget.style === "carousel" ? (focused ? 0 : -1) : undefined}
+          aria-focused={widget.style === "carousel" ? focused : undefined}
+          onKeyDown={onKeyDown}
+        >
+          <h2
+            id={mission._id}
+            className={`font-semibold h-[84px] line-clamp-3 text-xl group-hover:text-[#000091] group-focus-within:text-[#000091] transition-colors duration-300`}
           >
-            <h2 id={mission._id} className={`font-semibold line-clamp-3 text-xl group-hover:text-[#000091] group-focus-within:text-[#000091] transition-colors duration-300`}>
-              {mission.title}
-            </h2>
-          </a>
-          <p className={`text-sm line-clamp-1 text-[#3A3A3A] group-hover:line-clamp-4 group-focus-within:line-clamp-4`}>{address}</p>
-        </div>
+            {mission.title}
+          </h2>
+        </a>
+        <p className="text-sm line-clamp-1 text-[#3A3A3A] group-hover:line-clamp-2 group-focus-within:line-clamp-2 my-3">{address}</p>
 
-        <div className="min-h-[19px] flex items-center mt-auto">
-          {mission.tags?.includes("Service Civique Écologique") ? (
-            <Image src={LogoSCE} width="0" height="0" style={{ width: "228px", height: "19px" }} alt="Service Civique Écologique" />
-          ) : (
-            <div style={{ width: "228px", height: "19px" }} />
-          )}
-        </div>
-
-        <div className={`flex justify-between items-center text-[#666666] ${address.length > 40 ? "group-hover:hidden group-focus-within:hidden" : ""}`}>
-          <div className="flex items-center min-w-[120px]">
-            <RiCalendarEventFill className="h-4 flex-shrink-0" />
-            <p className="text-xs ml-2 whitespace-nowrap">Dès que possible</p>
-          </div>
-        </div>
+        {mission.tags?.includes("Service Civique Écologique") && <Image src={LogoSCE} height={18} alt="Service Civique Écologique" />}
       </div>
-      <div className="h-[0.3125rem] w-full absolute bottom-0 left-0 flex-shrink-0" style={{ backgroundColor: domain.color }} />
+
+      <div className={`flex items-center min-w-[120px] text-[#666666] ${address.length > 60 ? "group-hover:hidden group-focus-within:hidden" : ""}`}>
+        <RiCalendarEventFill className="h-5" />
+        <p className="text-xs ml-2 whitespace-nowrap leading-5">Dès que possible</p>
+      </div>
+
+      <div className="h-[5px] w-full absolute bottom-0 left-0" style={{ backgroundColor: domain.color }} />
     </div>
   );
 };
