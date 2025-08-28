2d2f760fd58b961953a9b4aa0b9c21eae71fce8f
theolemague <theo.lm@hotmail.fr>
2025-08-28 17:04:03 +0200

    config sentry widget

--- widget/instrumentation-client.js
+++ widget/instrumentation-client.js
@@ -1,11 +1,20 @@
-import * as Sentry from "@sentry/nextjs";
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import * as Sentry from "@sentry/nextjs";
 import { ENV, SENTRY_DSN } from "./config";
 
 if (ENV !== "development") {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: ENV,
-    tracesSampleRate: 1.0,
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
   });
 }
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- widget/sentry.edge.config.js
+++ widget/sentry.edge.config.js
@@ -1,11 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
-import { ENV, SENTRY_DSN } from "./config";
+Sentry.init({
+  dsn: "https://02d82167bede50a61cac46350d48c507@sentry.api-engagement.beta.gouv.fr/6",
 
-if (ENV !== "development") {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    environment: ENV,
-    tracesSampleRate: 1.0,
-  });
-}
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
