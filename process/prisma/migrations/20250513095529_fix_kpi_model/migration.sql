b2d531d670d0519e820972a85c7f1d98bb572123
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2025-09-02 15:40:49 +0200

    Delete legacy process folder

--- process/prisma/migrations/20250513095529_fix_kpi_model/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Kpi" ALTER COLUMN "available_jva_mission_count" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "KpiBotLess" ALTER COLUMN "available_jva_mission_count" DROP NOT NULL;
