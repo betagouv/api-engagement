0aaef8828b9c76d68c8fce2f722c67a7e8956391
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2025-09-10 08:04:08 +0200

    Init dual Prisma confiugration

--- api/src/db/postgres.ts
+++ api/src/db/postgres.ts
@@ -1,20 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient as PrismaClientAnalytics } from "./analytics";
+import { PrismaClient as PrismaClientCore } from "./core";
 
-const prisma = new PrismaClient({
+const prismaCore = new PrismaClientCore({
+  log: ["error"],
+});
+const prismaAnalytics = new PrismaClientAnalytics({
   log: ["error"],
 });
 
-export const pgConnected = new Promise<void>((resolve, reject) => {
-  prisma
-    .$connect()
-    .then(() => {
-      console.log("[PostgreSQL] Connected");
-      resolve();
-    })
-    .catch((error) => {
-      console.error("[PostgreSQL] Connection error:", error);
-      reject(error);
+export const pgConnected = Promise.all(
+  [prismaCore, prismaAnalytics].map((prisma) => {
+    return new Promise<void>((resolve, reject) => {
+      prisma
+        .$connect()
+        .then(() => {
+          console.log(`[PostgreSQL] ${prisma} connected`);
+          resolve();
+        })
+        .catch((error) => {
+          console.error(`[PostgreSQL] ${prisma} Connection error:`, error);
+          reject(error);
+        });
     });
-});
+  })
+);
 
-export default prisma;
+export default { prismaCore, prismaAnalytics };
