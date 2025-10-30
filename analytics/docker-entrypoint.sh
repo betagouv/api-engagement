2a948e9fb62b846d281d2d0ae4b3d061ca8eb462
Arnaud <arnaud.costes@gmail.com>
2025-10-30 09:56:13 +0100

    feat(analytics): setup dbt inside analytics docker image

--- analytics/docker-entrypoint.sh
+++ analytics/docker-entrypoint.sh
@@ -1,28 +1,4 @@
 #!/bin/sh
-
-set -e
-
-if [ -n "${DATABASE_URL_ANALYTICS:-}" ]; then
-  url="$DATABASE_URL_ANALYTICS"
-
-  creds="${url#*//}"         # user:pass@host:port/db
-  creds="${creds%@*}"        # user:pass
-  hostportdb="${url#*@}"     # host:port/db
-
-  user="${creds%%:*}"
-  pass="${creds#*:}"
-  hostport="${hostportdb%%/*}"
-  db="${hostportdb#*/}"
-  host="${hostport%%:*}"
-  port="${hostport#*:}"
-
-  export DBT_DB_USER="$user"
-  export DBT_DB_PASSWORD="$pass"
-  export DBT_DB_HOST="$host"
-  export DBT_DB_PORT="${port:-5432}"
-  export DBT_DB_NAME="${db%%\?*}"   # enlève ?sslmode=...
-fi
-
 echo "🚀 Running DB migration..."
 npm run db:migrate
 
