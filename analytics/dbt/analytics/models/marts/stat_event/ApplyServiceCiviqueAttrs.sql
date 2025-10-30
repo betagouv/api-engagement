5ea9505d323476269974461b8c2926df37c322a9
Arnaud <arnaud.costes@gmail.com>
2025-10-30 18:51:02 +0100

    feat(analytics): apply ServiceCivique custom attributes model

---
+++ analytics/dbt/analytics/models/marts/stat_event/ApplyServiceCiviqueAttrs.sql
@@ -0,0 +1,37 @@
+{{ config (
+  materialized = 'incremental',
+  unique_key = 'apply_id',
+) }}
+
+with src as (
+  select
+    event_id,
+    created_at,
+    custom_attributes::jsonb as attrs,
+    to_partner_id
+  from {{ ref('stg_stat_event__apply') }}
+  {% if is_incremental() %}
+    where
+      created_at
+      > (select coalesce(max(a.created_at), '1900-01-01') from {{ this }} as a)
+  {% endif %}
+),
+
+service_civique as (
+  select id from {{ ref('dim_partner') }}
+  where name = 'Service Civique'
+)
+
+select
+  s.event_id as apply_id,
+  s.created_at,
+  (s.attrs ->> 'hasApplicationFile')::boolean as has_application_file,
+  (s.attrs ->> 'hasCandidateMotivation')::boolean as has_candidate_motivation,
+  s.attrs ->> 'candidateAge' as candidate_age,
+  s.attrs ->> 'candidateGender' as candidate_gender,
+  s.attrs ->> 'candidatePostalCode' as candidate_postal_code
+from src as s
+inner join service_civique as sc on s.to_partner_id = sc.id
+where
+  s.attrs is not null
+  and s.attrs <> '{}'::jsonb
