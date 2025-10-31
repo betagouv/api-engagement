70fd527ad00ca287b98718d5c65d4f63d2929ecd
Arnaud <arnaud.costes@gmail.com>
2025-10-31 15:08:07 +0100

    refactor(analytics): add updatedAt field to dbt model for StatEvent

--- analytics/dbt/analytics/models/staging/stat_event/stg_stat_event__apply.sql
+++ analytics/dbt/analytics/models/staging/stat_event/stg_stat_event__apply.sql
@@ -39,6 +39,7 @@ mission_map as (
 select
   e.id as event_id,
   e.created_at,
+  e.updated_at,
   e.host,
   e.tag,
   p_from.id as from_partner_id,
--- analytics/dbt/analytics/models/staging/stat_event/stg_stat_event__print.sql
+++ analytics/dbt/analytics/models/staging/stat_event/stg_stat_event__print.sql
@@ -33,6 +33,7 @@ mission_map as (
 select
   e.id as event_id,
   e.created_at,
+  e.updated_at,
   e.host,
   p_from.id as from_partner_id,
   p_to.id as to_partner_id,
