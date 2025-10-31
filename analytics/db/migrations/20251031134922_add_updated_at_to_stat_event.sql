f5756df00d4042ab4e0bcba837aa8e6b6b34c7f4
Arnaud <arnaud.costes@gmail.com>
2025-10-31 16:20:04 +0100

    fix(analytics): no backfill for updatedAt keep it nullable

--- analytics/db/migrations/20251031134922_add_updated_at_to_stat_event.sql
+++ analytics/db/migrations/20251031134922_add_updated_at_to_stat_event.sql
@@ -1,7 +1,5 @@
 -- migrate:up
-ALTER TABLE "analytics_raw"."StatEvent" ADD COLUMN     "updated_at" TIMESTAMP(3);
-UPDATE "analytics_raw"."StatEvent" SET "updated_at" = "created_at" WHERE "updated_at" IS NULL;
-ALTER TABLE "analytics_raw"."StatEvent" ALTER COLUMN "updated_at" SET NOT NULL;
+ALTER TABLE "analytics_raw"."StatEvent" ADD COLUMN "updated_at" TIMESTAMP(3);
 
 -- migrate:down
 ALTER TABLE "analytics_raw"."StatEvent" DROP COLUMN "updated_at";
