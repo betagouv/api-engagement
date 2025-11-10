d78b350922b8596155e8e38ac487782ecc19a8c7
Arnaud <arnaud.costes@gmail.com>
2025-11-10 13:30:14 +0100

    fix(analytics): reduce dbt run complexity for apply table

--- analytics/dbt/analytics/models/marts/stat_event/mart_impression.sql
+++ analytics/dbt/analytics/models/marts/stat_event/mart_impression.sql
@@ -19,13 +19,6 @@ with src as (
       created_at
       > (select coalesce(max(i.created_at), '1900-01-01') from {{ this }} as i)
   {% endif %}
-),
-
-prepared as (
-  select
-    *,
-    md5(stat_event_id::text) as event_hash
-  from src
 )
 
 select
@@ -40,13 +33,5 @@ select
   to_publisher_id,
   mission_id,
   mission_id_raw,
-  updated_at,
-  concat_ws(
-    '-',
-    substr(event_hash, 1, 8),
-    substr(event_hash, 9, 4),
-    substr(event_hash, 13, 4),
-    substr(event_hash, 17, 4),
-    substr(event_hash, 21, 12)
-  ) as id
-from prepared
+  updated_at
+from src
