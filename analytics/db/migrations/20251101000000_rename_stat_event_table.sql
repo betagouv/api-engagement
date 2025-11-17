e0065b02336ba0a594ed59eb289f25b61d6a8203
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2025-11-17 15:09:59 +0100

    fix(analytics): permissive migration

--- analytics/db/migrations/20251101000000_rename_stat_event_table.sql
+++ analytics/db/migrations/20251101000000_rename_stat_event_table.sql
@@ -1,7 +1,23 @@
 -- migrate:up
 ALTER TABLE "analytics_raw"."StatEvent" RENAME TO "stat_event";
-UPDATE "analytics_raw"."pg_export_state" SET "key" = 'stat_event' WHERE "key" = 'StatEvent';
+DO $$
+BEGIN
+  UPDATE analytics_raw.pg_export_state
+  SET "key" = 'stat_event'
+  WHERE "key" = 'StatEvent';
+EXCEPTION WHEN undefined_table THEN
+  NULL;
+END
+$$;
 
 -- migrate:down
 ALTER TABLE "analytics_raw"."stat_event" RENAME TO "StatEvent";
-UPDATE "analytics_raw"."pg_export_state" SET "key" = 'StatEvent' WHERE "key" = 'stat_event';
+DO $$
+BEGIN
+  UPDATE analytics_raw.pg_export_state
+  SET "key" = 'StatEvent'
+  WHERE "key" = 'stat_event';
+EXCEPTION WHEN undefined_table THEN
+  NULL;
+END
+$$;
