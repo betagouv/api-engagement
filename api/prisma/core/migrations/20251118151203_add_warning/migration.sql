3aeaa55ce1b6cfb851be382b4311cb8e8ca6b818
theolemague <theo.lm@hotmail.fr>
2025-11-18 16:13:02 +0100

    update migration

--- api/prisma/core/migrations/20251118151203_add_warning/migration.sql
+++ api/prisma/core/migrations/20251118151203_add_warning/migration.sql
@@ -1,9 +1,3 @@
--- DropIndex
-DROP INDEX "public"."stats_event_from_publisher_created_at_idx";
-
--- DropIndex
-DROP INDEX "public"."stats_event_to_publisher_created_at_idx";
-
 -- CreateTable
 CREATE TABLE "public"."warning" (
     "id" TEXT NOT NULL,
@@ -32,12 +26,6 @@ CREATE INDEX "warning_fixed_idx" ON "public"."warning"("fixed");
 
 -- CreateIndex
 CREATE INDEX "warning_created_at_idx" ON "public"."warning"("created_at");
-
--- CreateIndex
-CREATE INDEX "stats_event_from_publisher_created_at_idx" ON "public"."StatEvent"("from_publisher_id", "created_at");
-
--- CreateIndex
-CREATE INDEX "stats_event_to_publisher_created_at_idx" ON "public"."StatEvent"("to_publisher_id", "created_at");
 
 -- AddForeignKey
 ALTER TABLE "public"."warning" ADD CONSTRAINT "warning_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "public"."publisher"("id") ON DELETE CASCADE ON UPDATE CASCADE;
