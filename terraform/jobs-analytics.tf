f0fd9b1b14fd702e3a28864646982fa51dd8f54f
Arnaud <arnaud.costes.pro@gmail.com>
2025-11-24 13:52:35 +0100

    feat(analytics): publisher pipeline (#565)
    * feat(analytics): publisher pipeline
    
    * feat(analytics): annonceur and diffuseur models

--- terraform/jobs-analytics.tf
+++ terraform/jobs-analytics.tf
@@ -86,6 +86,24 @@ resource "scaleway_job_definition" "analytics-mission-event" {
   })
 }
 
+resource "scaleway_job_definition" "analytics-publisher" {
+  name         = "analytics-${terraform.workspace}-publisher"
+  project_id   = var.project_id
+  cpu_limit    = 1000
+  memory_limit = 2048
+  image_uri    = local.image_analytics_uri
+  timeout      = "120m"
+
+  cron {
+    schedule = "0 3 * * *" # Every day at 3:00 AM
+    timezone = "Europe/Paris"
+  }
+
+  env = merge(local.common_analytics_env_vars, {
+    JOB_CMD = "node dist/jobs/run-job.js export-to-analytics-raw publisher"
+  })
+}
+
 resource "scaleway_job_definition" "analytics-dbt-run" {
   name         = "analytics-${terraform.workspace}-dbt-run"
   project_id   = var.project_id
