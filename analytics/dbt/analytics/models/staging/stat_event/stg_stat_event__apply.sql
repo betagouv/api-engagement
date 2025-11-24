3f4402141bb2bf30eaf7142fec523aaca2e0cb99
Arnaud <arnaud.costes@gmail.com>
2025-11-24 16:59:07 +0100

    refactor(analytics): migration to publisher mart

--- analytics/dbt/analytics/models/staging/stat_event/stg_stat_event__apply.sql
+++ analytics/dbt/analytics/models/staging/stat_event/stg_stat_event__apply.sql
@@ -3,9 +3,9 @@ with events as (
     e.*,
     nullif(e.mission_id, '') as mission_id_raw,
     nullif(e.mission_client_id, '') as mission_client_id_raw,
-    nullif(e.to_publisher_id, '') as to_publisher_id_raw,
-    nullif(e.from_publisher_id, '') as from_publisher_id_raw,
-    nullif(e.source_id, '') as source_id_raw,
+    nullif(e.to_publisher_id, '') as to_publisher_id_clean,
+    nullif(e.from_publisher_id, '') as from_publisher_id_clean,
+    nullif(e.source_id, '') as source_id_clean,
     nullif(e.click_id, '') as click_id_raw,
     nullif(e.status, '') as status_raw
   from {{ ref('stg_stat_event') }} as e
@@ -15,7 +15,7 @@ with events as (
 publishers as (
   select
     id,
-    publisher_id_raw
+    name
   from {{ ref('dim_publisher') }}
 ),
 
@@ -54,14 +54,14 @@ select
   e.updated_at,
   e.host,
   e.tag,
-  p_from.id as from_publisher_id,
-  p_to.id as to_publisher_id,
   mm.mission_id,
   clk.stat_event_id as click_id,
   e.click_id_raw as view_id_raw,
   e.status_raw as status,
   e.custom_attributes,
   mm.resolved_mission_id_raw,
+  coalesce(p_from.id, e.from_publisher_id_clean) as from_publisher_id,
+  coalesce(p_to.id, e.to_publisher_id_clean) as to_publisher_id,
   case
     when coalesce(e.source, 'publisher') in ('publisher', 'api') then 'api'
     else e.source
@@ -69,23 +69,23 @@ select
   case
     when e.source = 'campaign' then c_source.id
     when e.source = 'widget' then w_source.id
-    when e.source = 'publisher' then p_source.id
+    when e.source = 'publisher' then coalesce(p_source.id, e.source_id_clean)
   end as source_id,
   case when e.source = 'campaign' then c_source.id end as campaign_id,
   case when e.source = 'widget' then w_source.id end as widget_id
 from events as e
 left join
   publishers as p_from
-  on e.from_publisher_id_raw = p_from.publisher_id_raw
-left join publishers as p_to on e.to_publisher_id_raw = p_to.publisher_id_raw
+  on e.from_publisher_id_clean = p_from.id
+left join publishers as p_to on e.to_publisher_id_clean = p_to.id
 left join mission_map as mm on e.id = mm.stat_event_id
 left join
   publishers as p_source
-  on e.source = 'publisher' and e.source_id_raw = p_source.publisher_id_raw
+  on e.source = 'publisher' and e.source_id_clean = p_source.id
 left join
   campaigns as c_source
-  on e.source = 'campaign' and e.source_id_raw = c_source.campaign_id_raw
+  on e.source = 'campaign' and e.source_id_clean = c_source.campaign_id_raw
 left join
   widgets as w_source
-  on e.source = 'widget' and e.source_id_raw = w_source.widget_id_raw
+  on e.source = 'widget' and e.source_id_clean = w_source.widget_id_raw
 left join clicks as clk on e.click_id_raw = clk.stat_event_id
