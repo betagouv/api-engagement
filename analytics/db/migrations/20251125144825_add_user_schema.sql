659b6475935cbbbc763044d6e99d4d708495abb3
Arnaud <arnaud.costes@gmail.com>
2025-11-25 17:23:37 +0100

    refactor(analytics): pipeline user and user_publisher

---
+++ analytics/db/migrations/20251125144825_add_user_schema.sql
@@ -0,0 +1,15 @@
+-- migrate:up
+CREATE TABLE IF NOT EXISTS "analytics_raw"."user" (
+  "id" TEXT PRIMARY KEY,
+  "first_name" TEXT NOT NULL,
+  "last_name" TEXT,
+  "email" TEXT NOT NULL,
+  "role" TEXT NOT NULL,
+  "deleted_at" TIMESTAMP(3),
+  "last_activity_at" TIMESTAMP(3),
+  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS "analytics_raw"."user";
---
+++ analytics/dbt/analytics/models/staging/user/stg_user.sql
@@ -0,0 +1,11 @@
+select
+  id,
+  first_name,
+  last_name,
+  email,
+  role,
+  deleted_at::timestamp as deleted_at,
+  last_activity_at::timestamp as last_activity_at,
+  created_at::timestamp as created_at,
+  updated_at::timestamp as updated_at
+from {{ source('analytics_raw', 'user') }}
