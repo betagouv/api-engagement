5dc8a5c90336965c651db54daeec733b935519e5
Arnaud <arnaud.costes@gmail.com>
2025-12-01 16:08:30 +0100

    refactor(api): remove loginAt field from user

---
+++ api/prisma/core/migrations/20251201150726_drop_user_login_at/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `login_at` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."user" DROP COLUMN "login_at";
