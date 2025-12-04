ae2febc6121cc9810405eee7bffb70973b629982
Arnaud <arnaud.costes@gmail.com>
2025-12-04 16:41:34 +0100

    fix(analytics): wrong publisher_id join

--- analytics/dbt/analytics/models/staging/dim/dim_mission.sql
+++ analytics/dbt/analytics/models/staging/dim/dim_mission.sql
@@ -3,8 +3,9 @@ with base as (
     m.id,
     nullif(m.old_id, '') as mission_id_raw,
     nullif(m.client_id, '') as client_id,
-    nullif(m.partner_id::text, '') as publisher_id
+    nullif(p.id::text, '') as publisher_id
   from {{ source('public', 'Mission') }} as m
+  left join {{ ref('dim_publisher') }} as p on m.partner_id = p.partner_id
 )
 
 select * from base
