145ea128586dc4a69c1b8116118aa133c3dd79d8
Arnaud <arnaud.costes.pro@gmail.com>
2025-12-04 16:49:51 +0100

    fix(analytics): wrong publisher_id join (#624)

--- analytics/dbt/analytics/models/staging/dim/dim_publisher.sql
+++ analytics/dbt/analytics/models/staging/dim/dim_publisher.sql
@@ -1,4 +1,6 @@
 select
-  id,
-  name
-from {{ ref('stg_publisher') }}
+  pub.id,
+  p.id as partner_id,
+  pub.name
+from {{ ref('stg_publisher') }} as pub
+left join {{ source('public', 'Partner') }} as p on pub.id = p.old_id
