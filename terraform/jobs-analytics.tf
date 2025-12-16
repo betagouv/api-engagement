6e1106ccc12e05f9722ecc48cd389d52030d6f79
Arnaud <arnaud.costes.pro@gmail.com>
2025-12-16 15:01:30 +0100

    fix(analytics): missing widget terraform jobs (#632)

--- terraform/jobs-analytics.tf
+++ terraform/jobs-analytics.tf
@@ -248,6 +248,61 @@ resource "scaleway_job_definition" "analytics-campaign-tracker" {
   })
 }
 
+resource "scaleway_job_definition" "analytics-widget" {
+  name         = "analytics-${terraform.workspace}-widget"
+  project_id   = var.project_id
+  cpu_limit    = 1000
+  memory_limit = 2048
+  image_uri    = local.image_analytics_uri
+  timeout      = "120m"
+
+  cron {
+    schedule = "0 3 * * *" # Every day at 3:00 AM
+    timezone = "Europe/Paris"
+  }
+
+  env = merge(local.common_analytics_env_vars, {
+    JOB_CMD = "node dist/jobs/run-job.js export-to-analytics-raw widget"
+  })
+}
+
+resource "scaleway_job_definition" "analytics-widget-publisher" {
+  name         = "analytics-${terraform.workspace}-widget-publisher"
+  project_id   = var.project_id
+  cpu_limit    = 1000
+  memory_limit = 2048
+  image_uri    = local.image_analytics_uri
+  timeout      = "120m"
+
+  cron {
+    schedule = "0 3 * * *" # Every day at 3:00 AM
+    timezone = "Europe/Paris"
+  }
+
+  env = merge(local.common_analytics_env_vars, {
+    JOB_CMD = "node dist/jobs/run-job.js export-to-analytics-raw widget_publisher"
+  })
+}
+
+resource "scaleway_job_definition" "analytics-widget-rule" {
+  name         = "analytics-${terraform.workspace}-widget-rule"
+  project_id   = var.project_id
+  cpu_limit    = 1000
+  memory_limit = 2048
+  image_uri    = local.image_analytics_uri
+  timeout      = "120m"
+
+  cron {
+    schedule = "0 3 * * *" # Every day at 3:00 AM
+    timezone = "Europe/Paris"
+  }
+
+  env = merge(local.common_analytics_env_vars, {
+    JOB_CMD = "node dist/jobs/run-job.js export-to-analytics-raw widget_rule"
+  })
+}
+
+
 resource "scaleway_job_definition" "analytics-dbt-run" {
   name         = "analytics-${terraform.workspace}-dbt-run"
   project_id   = var.project_id
