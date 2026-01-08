70a9739d9cdf8269d1e918ed4744a32a0e3ddc54
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2026-01-08 12:08:37 +0100

    chore(ci): decrease import-missions frequency

--- terraform/jobs.tf
+++ terraform/jobs.tf
@@ -207,10 +207,10 @@ resource "scaleway_job_definition" "import-missions" {
   memory_limit = 2048
   image_uri    = local.image_uri
   command      = "node dist/jobs/run-job.js import-missions"
-  timeout      = "45m"
+  timeout      = "60m"
 
   cron {
-    schedule = "15 */3 * * *" # Every 3 hours at 15 minutes
+    schedule = "15 */6 * * *" # Every 6 hours at 15 minutes
     timezone = "Europe/Paris"
   }
 
