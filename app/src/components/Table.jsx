766b942968f0728e3027b7ee15d9a647ce6dfef3
theolemague <theo.lm@hotmail.fr>
2026-01-16 17:25:44 +0100

    refactor(app): a11y pagination

--- app/src/components/Table.jsx
+++ app/src/components/Table.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { BiFirstPage, BiLastPage } from "react-icons/bi";
+
 import { IoIosArrowDown } from "react-icons/io";
-import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
+import { RiArrowLeftSLine, RiArrowRightSLine, RiSkipLeftLine, RiSkipRightLine } from "react-icons/ri";
 
 import Loader from "./Loader";
 
@@ -54,108 +54,77 @@ const Table = ({ header, sortBy, total, onSort, loading, children, auto = false,
           </tbody>
         </table>
       </div>
-      {pagination && (
-        <div className="mt-3">
-          <Pagination page={page} setPage={onPageChange} end={Math.ceil(total / pageSize) || 1} />
-        </div>
-      )}
+      {pagination && <Pagination page={page} setPage={onPageChange} end={Math.ceil(total / pageSize) || 1} />}
     </>
   );
 };
 
+// https://www.systeme-de-design.gouv.fr/version-courante/fr/composants/pagination/code-de-la-pagination
 const Pagination = ({ page, setPage, end }) => {
-  const [pages, setPages] = useState([...Array(end).keys()].map((i) => i + 1));
+  const [pages, setPages] = useState([]);
 
   useEffect(() => {
     if (page < 1) setPage(1);
     if (page > end && end > 0) setPage(end);
-    setPages([...Array(end).keys()].map((i) => i + 1));
-  }, [end]);
+    if (end > 5) {
+      if (page < 4) {
+        setPages(["1", "2", "3", "4", "...", end.toString()]);
+      } else if (page > end - 3) {
+        setPages(["1", "...", end - 2, end - 1, end.toString()]);
+      } else {
+        setPages(["1", "...", page - 1, page, page + 1, "...", end.toString()]);
+      }
+    } else {
+      setPages([...Array(end).keys()].map((i) => (i + 1).toString()));
+    }
+  }, [end, page]);
 
   return (
-    <div className="flex flex-row items-center justify-center gap-1 text-sm">
-      <button
-        className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center disabled:opacity-60 disabled:hover:bg-transparent"
-        onClick={() => setPage(1)}
-        disabled={page === 1}
-      >
-        <BiFirstPage />
-      </button>
-      <button
-        className="hover:bg-gray-975 mr-4 flex h-8 items-center gap-1 px-2 disabled:opacity-60 disabled:hover:bg-transparent"
-        onClick={() => setPage(page - 1)}
-        disabled={page === 1}
-      >
-        <MdNavigateBefore className="mr-1" />
-        Page précédente
-      </button>
-      {end > 5 ? (
-        page < 4 ? (
-          <>
-            {pages.slice(0, 4).map((p) => (
-              <button key={p} className={`hover:bg-gray-975 flex h-8 w-8 items-center justify-center ${p === page ? "bg-blue-france text-white" : ""}`} onClick={() => setPage(p)}>
-                {p}
-              </button>
-            ))}
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center">...</button>
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center" onClick={() => setPage(end)}>
-              {end}
-            </button>
-          </>
-        ) : page > end - 3 ? (
-          <>
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center" onClick={() => setPage(1)}>
-              1
-            </button>
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center">...</button>
-            {pages.slice(end - 4, end).map((p) => (
-              <button key={p} className={`hover:bg-gray-975 flex h-8 w-8 items-center justify-center ${p === page ? "bg-blue-france text-white" : ""}`} onClick={() => setPage(p)}>
-                {p}
-              </button>
-            ))}
-          </>
-        ) : (
-          <>
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center" onClick={() => setPage(1)}>
-              1
-            </button>
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center">...</button>
-            {pages.slice(page - 2, page + 1).map((p) => (
-              <button key={p} className={`hover:bg-gray-975 flex h-8 w-8 items-center justify-center ${p === page ? "bg-blue-france text-white" : ""}`} onClick={() => setPage(p)}>
-                {p}
-              </button>
-            ))}
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center" disabled={true}>
-              ...
-            </button>
-            <button className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center" onClick={() => setPage(end)}>
-              {end}
-            </button>
-          </>
-        )
-      ) : (
-        pages.map((p) => (
-          <button key={p} className={`hover:bg-gray-975 flex h-8 w-8 items-center justify-center ${p === page ? "bg-blue-france text-white" : ""}`} onClick={() => setPage(p)}>
-            {p}
+    <nav role="navigation" aria-label="pagination" className="mt-4 flex flex-row items-center justify-center gap-1 text-sm">
+      <ul className="flex flex-row flex-wrap items-center justify-start text-sm">
+        <li>
+          <button aria-label="Première page" className="pagination-btn" onClick={() => setPage(1)} disabled={page === 1} aria-disabled={page === 1}>
+            <RiSkipLeftLine className="mt-1 text-base" aria-hidden="true" />
+            <span class="sr-only">Première page</span>
           </button>
-        ))
-      )}
-      <button
-        className="hover:bg-gray-975 ml-4 flex h-8 items-center gap-1 px-2 disabled:opacity-60 disabled:hover:bg-transparent"
-        onClick={() => setPage(page + 1)}
-        disabled={page === end}
-      >
-        Page suivante
-        <MdNavigateNext className="ml-1" />
-      </button>
-      <button
-        className="hover:bg-gray-975 flex h-8 w-8 items-center justify-center disabled:opacity-60 disabled:hover:bg-transparent"
-        onClick={() => setPage(end)}
-        disabled={page === end}
-      >
-        <BiLastPage />
-      </button>
-    </div>
+        </li>
+        <li>
+          <button className="pagination-btn" onClick={() => setPage(page - 1)} disabled={page === 1} aria-disabled={page === 1}>
+            <RiArrowLeftSLine className="mt-1 mr-2" aria-hidden="true" />
+            Page précédente
+          </button>
+        </li>
+
+        {pages.map((p, index) => (
+          <li key={index}>
+            {p === "..." ? (
+              <div className="mx-2 text-sm">{p}</div>
+            ) : (
+              <button
+                aria-label={`Page ${p}`}
+                aria-current={parseInt(p) === page ? "page" : undefined}
+                className={`pagination-btn ${parseInt(p) === page ? "bg-blue-france text-white" : ""}`}
+                onClick={() => setPage(parseInt(p))}
+              >
+                {p}
+              </button>
+            )}
+          </li>
+        ))}
+        <li>
+          <button aria-label="Page suivante" className="pagination-btn" onClick={() => setPage(page + 1)} disabled={page === end} aria-disabled={page === end}>
+            Page suivante
+            <RiArrowRightSLine className="mt-1 ml-2 text-base" aria-hidden="true" />
+          </button>
+        </li>
+        <li>
+          <button className="pagination-btn" onClick={() => setPage(end)} disabled={page === end} aria-label="Dernière page" aria-disabled={page === end}>
+            <RiSkipRightLine className="mt-1 text-base" aria-hidden="true" />
+            <span class="sr-only">Dernière page</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
