ba5aeee683347523007a78ded6bbf5780f071978
Arnaud <arnaud.costes.pro@gmail.com>
2026-01-27 10:11:58 +0100

    feat(analytics): active organization models (#713)
    * feat(analytics): active organization models
    
    * fix(analytics): update documentation

---
+++ analytics/dbt/analytics/models/marts/organization/organization_active_monthly.sql
@@ -0,0 +1,91 @@
+{{ config(
+  materialized = 'incremental',
+  unique_key = ['year', 'month', 'is_all_department', 'department', 'publisher_category'],
+  incremental_strategy = 'delete+insert',
+  on_schema_change = 'sync_all_columns'
+) }}
+
+with last_run as (
+  {% if is_incremental() %}
+    select coalesce(max(month_start), '1900-01-01'::date) as last_month_start
+    from {{ this }}
+  {% else %}
+    select '1900-01-01'::date as last_month_start
+  {% endif %}
+),
+
+base as (
+  select
+    extract(year from active_date)::int as year,
+    extract(month from active_date)::int as month,
+    date_trunc('month', active_date)::date as month_start,
+    department,
+    publisher_category,
+    organization_id
+  from {{ ref('int_organization_active_daily') }}
+  {% if is_incremental() %}
+    where active_date >= (select lr.last_month_start from last_run as lr)
+  {% endif %}
+),
+
+dept as (
+  select
+    year,
+    month,
+    month_start,
+    false as is_all_department,
+    department,
+    publisher_category,
+    count(distinct organization_id) as organization_count
+  from base
+  where department is not null
+  group by year, month, month_start, department, publisher_category
+),
+
+dept_all_category as (
+  select
+    year,
+    month,
+    month_start,
+    false as is_all_department,
+    department,
+    'all' as publisher_category,
+    count(distinct organization_id) as organization_count
+  from base
+  where department is not null
+  group by year, month, month_start, department
+),
+
+all_dept as (
+  select
+    year,
+    month,
+    month_start,
+    true as is_all_department,
+    null as department,
+    publisher_category,
+    count(distinct organization_id) as organization_count
+  from base
+  group by year, month, month_start, publisher_category
+),
+
+all_dept_all_category as (
+  select
+    year,
+    month,
+    month_start,
+    true as is_all_department,
+    null as department,
+    'all' as publisher_category,
+    count(distinct organization_id) as organization_count
+  from base
+  group by year, month, month_start
+)
+
+select * from dept
+union all
+select * from dept_all_category
+union all
+select * from all_dept
+union all
+select * from all_dept_all_category
