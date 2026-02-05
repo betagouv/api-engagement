cfe4f6c11f03cb7140d915cbfc1dc8488ddcead0
theolemague <60031565+theolemague@users.noreply.github.com>
2026-02-05 14:43:31 +0100

    refactor(jobs,app,widget): change api adresse dns (#756)

--- api/src/services/data-gouv/api.ts
+++ api/src/services/data-gouv/api.ts
@@ -17,21 +17,4 @@ const get = async <T>(path: string) => {
   }
 };
 
-export const getAddressCsv = async (csv: string) => {
-  try {
-    const formData = new FormData();
-    // transform csv string to blob
-    const blob = new Blob([csv], { type: "text/csv" });
-    formData.append("data", blob, "data.csv");
-    const res = await fetch(`https://api-adresse.data.gouv.fr/search/csv`, {
-      method: "POST",
-      body: formData,
-    });
-    return await res.text();
-  } catch (error) {
-    captureException(error);
-    return null;
-  }
-};
-
-export default { get, getAddressCsv };
+export default { get };
