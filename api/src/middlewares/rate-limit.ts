28a6d3f9c5d4da67059685b08413e93e60035e36
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2026-03-05 17:05:06 +0100

    fix(api): tests

--- api/src/middlewares/rate-limit.ts
+++ api/src/middlewares/rate-limit.ts
@@ -1,9 +1,9 @@
-import { rateLimit } from "express-rate-limit";
+import { ipKeyGenerator, rateLimit } from "express-rate-limit";
 
 export const defaultRateLimiter = rateLimit({
   windowMs: 60_000,
   limit: 60,
-  keyGenerator: (req) => (req.headers["x-api-key"] as string) || (req.headers["apikey"] as string) || req.ip || "unknown",
+  keyGenerator: (req) => (req.headers["x-api-key"] as string) || (req.headers["apikey"] as string) || ipKeyGenerator(req),
   handler: (req, res) => {
     res.status(429).send({
       ok: false,
