d2df8f5124309971a9ef8496f8061d0b9def66f1
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2026-03-09 15:12:46 +0100

    fix(tests): mock s3

--- api/tests/mocks/index.ts
+++ api/tests/mocks/index.ts
@@ -1,5 +1,6 @@
 import dataSubventionMock from "./dataSubventionMock";
 import pgMock, { createPrismaClientMock, createPrismaCrudMock } from "./pgMock";
+import s3Mock from "./s3Mock";
 import sentryMock from "./sentryMock";
 
-export { createPrismaClientMock, createPrismaCrudMock, dataSubventionMock, pgMock, sentryMock };
+export { createPrismaClientMock, createPrismaCrudMock, dataSubventionMock, pgMock, s3Mock, sentryMock };
