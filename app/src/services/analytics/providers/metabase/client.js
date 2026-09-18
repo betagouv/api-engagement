376c786f88221ccb6c42a3de0cfc6a6183a273de
Arnaud <arnaud.costes.pro@gmail.com>
2026-09-18 09:34:01 +0200

    fix(api): prevent Metabase publisher scope bypass (#1482)

--- app/src/services/analytics/providers/metabase/client.js
+++ app/src/services/analytics/providers/metabase/client.js
@@ -1,8 +1,7 @@
 import api from "@/services/api";
 
-export const queryCard = async (cardId, { parameters = [], variables = {}, body = {}, signal } = {}) => {
-  const payload = { ...body };
-  if (parameters?.length) payload.parameters = parameters;
+export const queryCard = async (cardId, { variables = {}, signal } = {}) => {
+  const payload = {};
   if (variables && Object.keys(variables).length) payload.variables = variables;
 
   return api.post(`/metabase/card/${cardId}/query`, payload, { signal });
